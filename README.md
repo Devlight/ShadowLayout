c28153905979661335d54d21bfa8a563c267d9bb
GIGAMOLE <gigamole53@gmail.com>
2016-04-25 15:05:01 +0300

    Hand on

---
+++ README.md
@@ -0,0 +1,127 @@
+
+ShadowLayout
+===================
+
+This library allows you to create a shadow effect for your layout based on your child.
+
+![](https://lh4.googleusercontent.com/-2JB-2cEv8lk/Vx4FmHQhjOI/AAAAAAAACTA/nrRGFjcQXBsGiISYSZ5k8gUsVcRw5GSRQCL0B/w349-h552-no/sl.png)
+
+U can check the sample app [here](https://github.com/DevLight-Mobile-Agency/ShadowLayout/tree/master/app).
+
+Download
+------------
+
+You can download a .aar` from GitHub's [releases page](https://github.com/DevLight-Mobile-Agency/ShadowLayout/releases).
+
+Or use Gradle jCenter:
+
+```groovy
+dependencies {
+    repositories {
+        mavenCentral()
+        maven {
+            url  'http://dl.bintray.com/gigamole/maven/'
+        }
+    }
+    compile 'com.github.devlight.shadowlayout:library:+'
+}
+```
+
+Or Gradle Maven Central:
+
+```groovy
+compile 'com.github.devlight.shadowlayout:library:1.0.0'
+```
+
+Or Maven:
+
+```groovy
+<dependency>
+    <groupId>com.github.devlight.shadowlayout</groupId>
+    <artifactId>library</artifactId>
+    <version>1.0.0</version>
+    <type>aar</type>
+</dependency>
+```
+
+Android SDK Version
+=========
+
+ShadowLayout requires a minimum sdk version of 11. 
+
+Sample
+========
+
+For ShadowLayout you can set such parameters as:
+ 
+ - shadowed:
+    
+    allows you to handle shadow visibility.
+    
+ - shadow distance:
+     
+    allows you to set distance of shadow.    
+
+ - shadow angle:
+    
+    allows you to set shadow angle.
+    
+ - shadow radius:
+     
+    allows you to set shadow radius.
+     
+ - shadow color:
+     
+    allows you to set shadow color.
+
+Angle can only be positive and be in range from 0 to 360 degrees.
+
+ShadowLayout automatically set padding for shadow space draw.
+
+Check out in code init:
+
+```java
+final ShadowLayout shadowLayout = (ShadowLayout) findViewById(R.id.sl);
+shadowLayout.setIsShadowed(true);
+shadowLayout.setShadowAngle(45);
+shadowLayout.setShadowRadius(20);
+shadowLayout.setShadowDistance(30);
+shadowLayout.setShadowColor(Color.DKGRAY);
+```
+            
+Other methods check out in sample.
+
+And XML init:
+
+```xml
+<com.gigamole.samples.ShadowLayout
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    app:sl_shadowed="true"
+    app:sl_shadow_angle="45"
+    app:sl_shadow_radius="20dp"
+    app:sl_shadow_distance="30dp"
+    app:sl_shadow_color="#000">
+    
+    <!-- Set your child`s-->
+    
+</com.gigamole.samples.ShadowLayout>
+```
+
+Getting Help
+======
+
+To report a specific problem or feature request, [open a new issue on Github](https://github.com/DevLight-Mobile-Agency/ShadowLayout/issues/new).
+
+License
+======
+
+Apache 2.0 and MIT. See [LICENSE](https://github.com/DevLight-Mobile-Agency/ShadowLayout/blob/master/LICENSE.txt) file for details.
+
+
+Author
+=======
+
+Made in [DevLight Mobile Agency](https://github.com/DevLight-Mobile-Agency)
+
+Created by [Basil Miller](https://github.com/GIGAMOLE) - [@gigamole](mailto:http://gigamole53@gmail.com)
---
+++ library/src/main/java/com/gigamole/library/ShadowLayout.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright (C) 2015 Basil Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gigamole.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.FloatRange;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by GIGAMOLE on 13.04.2016.
+ */
+public class ShadowLayout extends FrameLayout {
+
+    // Default shadow values
+    private final static float DEFAULT_SHADOW_RADIUS = 30.0f;
+    private final static float DEFAULT_SHADOW_DISTANCE = 15.0f;
+    private final static float DEFAULT_SHADOW_ANGLE = 45.0f;
+    private final static int DEFAULT_SHADOW_COLOR = Color.DKGRAY;
+
+    // Shadow bounds values
+    private final static float MAX_ANGLE = 360.0f;
+    private final static float MIN_RADIUS = 0.1f;
+    private final static float MIN_ANGLE = 0.0f;
+
+    // Shadow bitmap and canvas
+    private Bitmap mBitmap;
+    private Canvas mCanvas = new Canvas();
+
+    // View bounds
+    private Rect mBounds = new Rect();
+    // Shadow paint
+    private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG) {
+        {
+            setDither(true);
+            setFilterBitmap(true);
+        }
+    };
+
+    // Check whether need to redraw shadow
+    private boolean mInvalidateShadow = true;
+
+    // Detect if shadow is visible
+    private boolean mIsShadowed;
+
+    // Shadow variables
+    private int mShadowColor;
+    private float mShadowRadius;
+    private float mShadowDistance;
+    private float mShadowAngle;
+    private float mShadowDx;
+    private float mShadowDy;
+
+    public ShadowLayout(final Context context) {
+        this(context, null);
+    }
+
+    public ShadowLayout(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ShadowLayout(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        setWillNotDraw(false);
+        setLayerType(LAYER_TYPE_HARDWARE, mPaint);
+
+        // Retrieve attributes from xml
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ShadowLayout);
+        try {
+            setIsShadowed(typedArray.getBoolean(R.styleable.ShadowLayout_sl_shadowed, true));
+            setShadowRadius(
+                    typedArray.getDimension(
+                            R.styleable.ShadowLayout_sl_shadow_radius, DEFAULT_SHADOW_RADIUS
+                    )
+            );
+            setShadowDistance(
+                    typedArray.getDimension(
+                            R.styleable.ShadowLayout_sl_shadow_distance, DEFAULT_SHADOW_DISTANCE
+                    )
+            );
+            setShadowAngle(
+                    typedArray.getInteger(
+                            R.styleable.ShadowLayout_sl_shadow_angle, (int) DEFAULT_SHADOW_ANGLE
+                    )
+            );
+            setShadowColor(
+                    typedArray.getColor(
+                            R.styleable.ShadowLayout_sl_shadow_color, DEFAULT_SHADOW_COLOR
+                    )
+            );
+        } finally {
+            typedArray.recycle();
+        }
+    }
+
+    public boolean isShadowed() {
+        return mIsShadowed;
+    }
+
+    public void setIsShadowed(final boolean isShadowed) {
+        mIsShadowed = isShadowed;
+        postInvalidate();
+    }
+
+    public float getShadowDistance() {
+        return mShadowDistance;
+    }
+
+    public void setShadowDistance(final float shadowDistance) {
+        mShadowDistance = shadowDistance;
+        resetShadow();
+    }
+
+    public float getShadowAngle() {
+        return mShadowAngle;
+    }
+
+    @FloatRange
+    public void setShadowAngle(@FloatRange(from = MIN_ANGLE, to = MAX_ANGLE) final float shadowAngle) {
+        mShadowAngle = Math.max(MIN_ANGLE, Math.min(shadowAngle, MAX_ANGLE));
+        resetShadow();
+    }
+
+    public float getShadowRadius() {
+        return mShadowRadius;
+    }
+
+    public void setShadowRadius(final float shadowRadius) {
+        mShadowRadius = Math.max(MIN_RADIUS, shadowRadius);
+
+        if (isInEditMode()) return;
+        // Set blur filter to paint
+        mPaint.setMaskFilter(new BlurMaskFilter(mShadowRadius, BlurMaskFilter.Blur.NORMAL));
+        resetShadow();
+    }
+
+    public int getShadowColor() {
+        return mShadowColor;
+    }
+
+    public void setShadowColor(final int shadowColor) {
+        mShadowColor = shadowColor;
+
+        mPaint.setColor(shadowColor);
+        resetShadow();
+    }
+
+    public float getShadowDx() {
+        return mShadowDx;
+    }
+
+    public float getShadowDy() {
+        return mShadowDy;
+    }
+
+    // Reset shadow layer
+    private void resetShadow() {
+        // Detect shadow axis offset
+        mShadowDx = (float) ((mShadowDistance) * Math.cos(mShadowAngle / 180.0f * Math.PI));
+        mShadowDy = (float) ((mShadowDistance) * Math.sin(mShadowAngle / 180.0f * Math.PI));
+
+        // Set padding for shadow bitmap
+        final int padding = (int) (mShadowDistance + mShadowRadius);
+        setPadding(padding, padding, padding, padding);
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        // Set ShadowLayout bounds
+        mBounds.set(
+                0, 0, MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec)
+        );
+    }
+
+    @Override
+    public void requestLayout() {
+        // Redraw shadow
+        mInvalidateShadow = true;
+        super.requestLayout();
+    }
+
+    @Override
+    protected void dispatchDraw(final Canvas canvas) {
+        // If is not shadowed, skip
+        if (mIsShadowed) {
+            // If need to redraw shadow
+            if (mInvalidateShadow) {
+                // If bounds is zero
+                if (mBounds.width() != 0 && mBounds.height() != 0) {
+                    // Reset bitmap to bounds
+                    mBitmap = Bitmap.createBitmap(
+                            mBounds.width(), mBounds.height(), Bitmap.Config.ARGB_8888
+                    );
+                    // Canvas reset
+                    mCanvas.setBitmap(mBitmap);
+
+                    // We just redraw
+                    mInvalidateShadow = false;
+                    // Main feature of this lib. We create the local copy of all content, so now
+                    // we can draw bitmap as a bottom layer of natural canvas.
+                    // We draw shadow like blur effect on bitmap, cause of setShadowLayer() method of
+                    // paint does`t draw shadow, it draw another copy of bitmap
+                    super.dispatchDraw(mCanvas);
+
+                    // Draw alpha chanel bitmap of our local canvas
+                    mCanvas.drawBitmap(mBitmap.extractAlpha(), mShadowDx, mShadowDy, mPaint);
+                } else {
+                    // Clear shadow bitmap
+                    mBitmap.recycle();
+                    mBitmap = null;
+                }
+            }
+
+            // Draw shadow bitmap
+            if (mCanvas != null && mBitmap != null) canvas.drawBitmap(mBitmap, 0.0f, 0.0f, null);
+        }
+
+        // Draw child`s
+        super.dispatchDraw(canvas);
+    }
+}
